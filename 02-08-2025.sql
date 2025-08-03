7f9ddb6fb4fc510d8dc43882f652cc55905914be
vishwa-cyber-alt <60313865+vishwa-cyber-alt@users.noreply.github.com>
2025-08-03 21:42:07 +0530

    Update 02-08-2025.sql

--- 02-08-2025.sql
+++ 02-08-2025.sql
@@ -13,3 +13,11 @@ select * from Customers where first_name Like'J%'
 return last_name ends with e
 --------------------------------
 select * from Customers where last_name Like'%e'
+
+return age greater than 18
+--------------------------------
+select * Customers where age>18
+  
+return how many persons have age greater than 18
+---------------------------------------------------
+select count(*) Customers where age>18
